4b7c73460d1f364a7579176af0c9e71676e4ae78
pava-laptop <240204@studierende.htl-donaustadt.at>
2026-03-12 15:31:03 +0100

    Add Score Parser and ToString

---
+++ A3 - Snake - Angabe/Leaderboard.cs
@@ -0,0 +1,20 @@
+﻿namespace A3___Snake___Angabe;
+internal class Leaderboard
+{
+    private static string path = @"./highscore.txt";
+    public Score[] scores;
+    public Leaderboard()
+    {
+        if (!File.Exists(path))
+            File.Create(path).Dispose();
+        int scoreCount = 0;
+        using (StreamReader sr = new StreamReader(path))
+        {
+            while (!sr.EndOfStream)
+            {
+                scoreCount++;
+
+            }
+        }
+    }
+}
--- A3 - Snake - Angabe/Score.cs
+++ A3 - Snake - Angabe/Score.cs
@@ -1,11 +1,12 @@
-﻿namespace A3___Snake___Angabe
+﻿using System.Globalization;
+
+namespace A3___Snake___Angabe
 {
     /// <summary>
-    /// Class <c>Score</c> saves a score with Name and points.
+    /// Class <c>Score</c> saves a score with points and time.
     /// </summary>
     internal class Score
     {
-        public string Name { get; set; }
         private DateTime _date;
         private byte _points;
 
@@ -15,29 +16,39 @@
             init { _points = value; }
         }
         /// <summary>
-        /// If <paramref name="name"/> is null or not given, the screen will be cleared
+        /// If <paramref name="points"/> is 0 or not given, an empty score will be saved
         /// </summary>
         /// <param name="points"></param>
-        /// <param name="name"></param>
-        public Score(byte points = 0, string name = null)
+        public Score(byte points = 0)
         {
             Points = points;
             _date = DateTime.Now;
-            if (name != null) Name = name;
-            else
-            {
-                throw new NotImplementedException();
-                Console.Clear();
-                Console.WriteLine("Please enter your Name: ");
-                Console.ReadLine();
-            }
         }
         /// <summary>
         /// Method <c>ToString</c><br></br><br></br> Returns the current score as a string
         /// </summary>
         public override string ToString()
         {
-            return $"{null:f2} {Name:f15} {_date.Date}; {_date.TimeOfDay}";
+            if (Points !=0)
+                return $"{Points:d3};{_date.ToString("dd.MM.yyyy HH:mm:ss")}";
+            return $"---;--.--.---- --:--:--";
+        }
+        /// <summary>
+        /// Parses a span of UTF-8 characters into a value.<br/><br/>Accepted Format: points;dd.MM.yyyy HH:mm:ss
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static Score Parse(string s)
+        {
+            if (s.IndexOf('-') != -1)
+            {
+                string[] splitS = s.Split(';');
+                byte points = byte.Parse(splitS[0]);
+                Score score = new Score(points);
+                score._date = DateTime.ParseExact(splitS[1], format: "dd.MM.yyyy HH:mm:ss", CultureInfo.GetCultureInfo("de-AT"));
+                return score;
+            }
+            else return new();
         }
     }
 }
