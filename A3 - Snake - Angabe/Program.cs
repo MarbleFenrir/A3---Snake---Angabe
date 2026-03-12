aad5e561a4664817596d53faa3683b471250aaa0
sebas <sebas@LAPTOP-Q3U4JV7B>
2026-03-12 09:48:19 +0100

    Projektdateien hinzufügen.

---
+++ A3 - Snake - Angabe/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace A3___Snake___Angabe
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int spalte = 0;
+            int zeile = 0;
+            
+            while(true)
+            {
+                ConsoleKeyInfo key = Console.ReadKey(intercept: true);
+                switch (key.Key)
+                {
+                    case ConsoleKey.RightArrow:
+                        spalte++;
+                        Console.SetCursorPosition(spalte, zeile);
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        spalte--;
+                        Console.SetCursorPosition(spalte, zeile);
+                        break;
+                    case ConsoleKey.UpArrow:
+                        zeile--;
+                        Console.SetCursorPosition(spalte, zeile);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        zeile++;
+                        Console.SetCursorPosition(spalte, zeile);
+                        break;
+                }
+            }
+            
+        }
+    }
+}
