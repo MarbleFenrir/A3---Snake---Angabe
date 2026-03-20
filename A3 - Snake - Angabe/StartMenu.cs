e451a4fe6864d0f0bb1adfff1a86756fe9098e19
Pava <philipp.prinz.schule@gmail.com>
2026-03-20 21:30:54 +0100

    Fix #5

--- A3 - Snake - Angabe/StartMenu.cs
+++ A3 - Snake - Angabe/StartMenu.cs
@@ -1,3 +1,261 @@
+namespace A3___Snake___Angabe;
+
+internal class StartMenu
+{
+    private static int selectedIndex = 0;
+    
+    private static readonly string[] options =
+    {
+        "Start Game",
+        "Leaderboard",
+        "Quit"
+    };
+    private static readonly string[] title =
+    {
+        @"  /$$$$$$  /$$   /$$  /$$$$$$  /$$   /$$ /$$$$$$$$",
+        @" /$$__  $$| $$$ | $$ /$$__  $$| $$  /$$/| $$_____/",
+        @"| $$  \__/| $$$$| $$| $$  \ $$| $$ /$$/ | $$      ",
+        @"|  $$$$$$ | $$ $$ $$| $$$$$$$$| $$$$$/  | $$$$$   ",
+        @" \____  $$| $$  $$$$| $$__  $$| $$  $$  | $$__/   ",
+        @" /$$  \ $$| $$\  $$$| $$  | $$| $$\  $$ | $$      ",
+        @"|  $$$$$$/| $$ \  $$| $$  | $$| $$ \  $$| $$$$$$$$",
+        @" \______/ |__/  \__/|__/  |__/|__/  \__/|________/"
+    };
+    
+    public static int Show()
+    {
+        ConsoleKey key;
+        do
+        {
+            DrawMenu();
+            
+            var keyInfo = Console.ReadKey(true);
+            key = keyInfo.Key;
+            
+            switch (key)
+            {
+                case ConsoleKey.UpArrow:
+                    selectedIndex = (selectedIndex - 1 + options.Length) % options.Length;
+                    break;
+                    
+                case ConsoleKey.DownArrow:
+                    selectedIndex = (selectedIndex + 1) % options.Length;
+                    break;
+                    
+                case ConsoleKey.Enter:
+                    return selectedIndex;
+                    
+                case ConsoleKey.Escape:
+                    return 2;
+            }
+        } while (true);
+    }
+    
+    private static void DrawMenu()
+    {
+        Console.Clear();
+        Console.CursorVisible = false;
+        if (Console.WindowHeight > 15)
+        {
+            for (int i = 0; i < title.Length; i++)
+                Game.WriteCenteredText(title[i], i + Console.WindowHeight / 7);
+        }
+        else Game.WriteCenteredText("SNAKE", 1);
+        
+        int windowWidth = Console.WindowWidth;
+        int windowHeight = Console.WindowHeight;
+        
+        for (int i = 0; i < options.Length; i++)
+        {
+            string text = options[i];
+            int x = (windowWidth - text.Length) / 2;
+            int y = (windowHeight / 2 - options.Length / 2) + i;
+            
+            Console.SetCursorPosition(x, y);
+            
+            if (i == selectedIndex)
+            {
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.WriteLine(text);
+                Console.ResetColor();
+            }
+            else Console.WriteLine(text);
+        }
+    }
+}
+namespace A3___Snake___Angabe;
+
+internal class StartMenu
+{
+    private static int selectedIndex = 0;
+    
+    private static readonly string[] options =
+    {
+        "Start Game",
+        "Leaderboard",
+        "Quit"
+    };
+    private static readonly string[] title =
+    {
+        @"  /$$$$$$  /$$   /$$  /$$$$$$  /$$   /$$ /$$$$$$$$",
+        @" /$$__  $$| $$$ | $$ /$$__  $$| $$  /$$/| $$_____/",
+        @"| $$  \__/| $$$$| $$| $$  \ $$| $$ /$$/ | $$      ",
+        @"|  $$$$$$ | $$ $$ $$| $$$$$$$$| $$$$$/  | $$$$$   ",
+        @" \____  $$| $$  $$$$| $$__  $$| $$  $$  | $$__/   ",
+        @" /$$  \ $$| $$\  $$$| $$  | $$| $$\  $$ | $$      ",
+        @"|  $$$$$$/| $$ \  $$| $$  | $$| $$ \  $$| $$$$$$$$",
+        @" \______/ |__/  \__/|__/  |__/|__/  \__/|________/"
+    };
+    
+    public static int Show()
+    {
+        ConsoleKey key;
+        do
+        {
+            DrawMenu();
+            
+            var keyInfo = Console.ReadKey(true);
+            key = keyInfo.Key;
+            
+            switch (key)
+            {
+                case ConsoleKey.UpArrow:
+                    selectedIndex = (selectedIndex - 1 + options.Length) % options.Length;
+                    break;
+                    
+                case ConsoleKey.DownArrow:
+                    selectedIndex = (selectedIndex + 1) % options.Length;
+                    break;
+                    
+                case ConsoleKey.Enter:
+                    return selectedIndex;
+                    
+                case ConsoleKey.Escape:
+                    return 2;
+            }
+        } while (true);
+    }
+    
+    private static void DrawMenu()
+    {
+        Console.Clear();
+        Console.CursorVisible = false;
+        if (Console.WindowHeight > 15)
+        {
+            for (int i = 0; i < title.Length; i++)
+                Game.WriteCenteredText(title[i], i + Console.WindowHeight / 7);
+        }
+        else Game.WriteCenteredText("SNAKE", 1);
+        
+        int windowWidth = Console.WindowWidth;
+        int windowHeight = Console.WindowHeight;
+        
+        for (int i = 0; i < options.Length; i++)
+        {
+            string text = options[i];
+            int x = (windowWidth - text.Length) / 2;
+            int y = (windowHeight / 2 - options.Length / 2) + i;
+            
+            Console.SetCursorPosition(x, y);
+            
+            if (i == selectedIndex)
+            {
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.WriteLine(text);
+                Console.ResetColor();
+            }
+            else Console.WriteLine(text);
+        }
+    }
+}
+namespace A3___Snake___Angabe;
+
+internal class StartMenu
+{
+    private static int selectedIndex = 0;
+    
+    private static readonly string[] options =
+    {
+        "Start Game",
+        "Leaderboard",
+        "Quit"
+    };
+    private static readonly string[] title =
+    {
+        @"  /$$$$$$  /$$   /$$  /$$$$$$  /$$   /$$ /$$$$$$$$",
+        @" /$$__  $$| $$$ | $$ /$$__  $$| $$  /$$/| $$_____/",
+        @"| $$  \__/| $$$$| $$| $$  \ $$| $$ /$$/ | $$      ",
+        @"|  $$$$$$ | $$ $$ $$| $$$$$$$$| $$$$$/  | $$$$$   ",
+        @" \____  $$| $$  $$$$| $$__  $$| $$  $$  | $$__/   ",
+        @" /$$  \ $$| $$\  $$$| $$  | $$| $$\  $$ | $$      ",
+        @"|  $$$$$$/| $$ \  $$| $$  | $$| $$ \  $$| $$$$$$$$",
+        @" \______/ |__/  \__/|__/  |__/|__/  \__/|________/"
+    };
+    
+    public static int Show()
+    {
+        ConsoleKey key;
+        do
+        {
+            DrawMenu();
+            
+            var keyInfo = Console.ReadKey(true);
+            key = keyInfo.Key;
+            
+            switch (key)
+            {
+                case ConsoleKey.UpArrow:
+                    selectedIndex = (selectedIndex - 1 + options.Length) % options.Length;
+                    break;
+                    
+                case ConsoleKey.DownArrow:
+                    selectedIndex = (selectedIndex + 1) % options.Length;
+                    break;
+                    
+                case ConsoleKey.Enter:
+                    return selectedIndex;
+                    
+                case ConsoleKey.Escape:
+                    return 2;
+            }
+        } while (true);
+    }
+    
+    private static void DrawMenu()
+    {
+        Console.Clear();
+        Console.CursorVisible = false;
+        if (Console.WindowHeight > 15)
+        {
+            for (int i = 0; i < title.Length; i++)
+                Game.WriteCenteredText(title[i], i + Console.WindowHeight / 7);
+        }
+        else Game.WriteCenteredText("SNAKE", 1);
+        
+        int windowWidth = Console.WindowWidth;
+        int windowHeight = Console.WindowHeight;
+        
+        for (int i = 0; i < options.Length; i++)
+        {
+            string text = options[i];
+            int x = (windowWidth - text.Length) / 2;
+            int y = (windowHeight / 2 - options.Length / 2) + i;
+            
+            Console.SetCursorPosition(x, y);
+            
+            if (i == selectedIndex)
+            {
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.WriteLine(text);
+                Console.ResetColor();
+            }
+            else Console.WriteLine(text);
+        }
+    }
+}
 ﻿namespace A3___Snake___Angabe;
 
 internal class StartMenu
